7c5f7203ec6d86816993284c0de7a8c496380493
Jose Souza <j.alison955@gmail.com>
2025-07-24 21:17:39 -0400

    chore: add font poppins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
-const Header = ({children}) => {
-    return (
-        <h1 className="title">{children}</h1>
-    );
+const Header = ({ children }) => {
+  return <h1 className="title font-semibold">{children}</h1>;
 };
 export default Header;
