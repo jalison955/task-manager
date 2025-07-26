5727dcad727902466f5c8b49f49ec3b7d4d34327
Jose Souza <j.alison955@gmail.com>
2025-07-26 16:58:36 -0400

    refactor: include icon as prop to TaskListPeriod component

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,11 +1,14 @@
 import TasksListPeriod from './TasksListPeriod';
+import SunIcon from '../assets/icons/sun.svg?react';
+import CloudSunIcon from '../assets/icons/cloud-sun.svg?react';
+import MoonIcon from '../assets/icons/moon.svg?react';
 
 const TasksList = ({ tasks }) => {
   return (
     <div className="rounded-lg bg-white p-5 shadow-sm">
-      <TasksListPeriod period={'manhã'} tasks={tasks} />
-      <TasksListPeriod period={'tarde'} tasks={tasks} />
-      <TasksListPeriod period={'noite'} tasks={tasks} />
+      <TasksListPeriod period={'manhã'} icon={<SunIcon />} tasks={tasks} />
+      <TasksListPeriod period={'tarde'} icon={<CloudSunIcon />} tasks={tasks} />
+      <TasksListPeriod period={'noite'} icon={<MoonIcon />} tasks={tasks} />
     </div>
   );
 };
--- src/components/TasksListPeriod.jsx
+++ src/components/TasksListPeriod.jsx
@@ -1,16 +1,13 @@
 import TasksListItem from './TasksListItem';
-import SunIcon from '../assets/icons/sun.svg?react';
-import CloudSunIcon from '../assets/icons/cloud-sun.svg?react';
-import MoonIcon from '../assets/icons/moon.svg?react';
 
-const TasksListPeriod = ({ period, tasks }) => {
+const TasksListPeriod = ({ period, icon, tasks }) => {
   return (
     <div className="mb-6 space-y-3 last:mb-0">
-      <div className="flex items-center gap-2 border-b border-[hsl(240,20%,95%)] pb-1.5 text-sm font-medium text-[hsl(216,5%,60%)]">
-        {period === 'manhã' && <SunIcon />}
-        {period === 'tarde' && <CloudSunIcon />}
-        {period === 'noite' && <MoonIcon />}
-        <p>{period.charAt(0).toUpperCase() + period.slice(1)}</p>
+      <div className="flex items-center gap-2 border-b border-[hsl(240,20%,95%)] pb-1.5">
+        {icon}
+        <p className="text-sm font-medium text-[hsl(216,5%,60%)]">
+          {period.charAt(0).toUpperCase() + period.slice(1)}
+        </p>
       </div>
       {tasks
         .filter((task) => task.period === period)
