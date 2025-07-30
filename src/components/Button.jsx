d0dedb663494637dd2e343496170a1f582d2b860
Jose Souza <j.alison955@gmail.com>
2025-07-30 18:09:54 -0400

    feat: add size and secondary variant props to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,25 @@
-const Button = ({ children, variant = 'primary', ...rest }) => {
+const Button = ({
+  children,
+  size = 'default',
+  variant = 'primary',
+  className = '',
+  ...rest
+}) => {
   const buttonStyles = {
     primary: 'bg-[hsl(183,100%,35%)] text-white',
+    secondary: 'bg-[hsl(220,10%,95%)] text-[hsl(220,10%,25%)]',
     ghost: 'bg-transparent text-[hsl(0,0%,50%)]',
   };
-  const styles = buttonStyles[variant];
+  const buttonSizes = {
+    default: 'py-1 text-xs',
+    large: 'py-2 text-sm',
+  };
+
+  const styles = `${buttonStyles[variant]} ${buttonSizes[size]} ${className}`;
 
   return (
     <button
-      className={`flex cursor-pointer items-center gap-1 rounded-md px-3 py-1 text-xs font-medium transition hover:opacity-75 ${styles} `}
+      className={`flex cursor-pointer items-center justify-center gap-1 rounded-md px-3 font-medium transition hover:opacity-75 ${styles} `}
       {...rest}
     >
       {children}
